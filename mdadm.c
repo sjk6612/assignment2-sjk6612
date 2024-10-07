f61bb1faaa1c666c585165669007b9a0f1ca1881
Sidhanth Karpe <sjk6612@psu.edu>
2024-10-07 18:29:31 -0400

    edit 1

--- mdadm.c
+++ mdadm.c
@@ -4,8 +4,34 @@
 #include "mdadm.h"
 #include "jbod.h"
 
+//disk size == address
+// create operation
+// add == block size
+// edge cases to worry about: you can only read 4 blocks at a time
+// if you are inside the block of a block, you want to make sure that you are not in a differnt block
+
+
+static int mounted_status = 0;
+
+uint32_t create_op(uint32_t DiskID, uint32_t BlockID, uint32_t Command, uint32_t Reserved) {
+  //DiskID
+  uint32_t op = 0; // op = 0
+  op = op | Command; // op = 1111
+  op = op << 8; // 1111 00000000
+  op = op | BlockID; // 1111 11111111
+  op = op << 8; // 1111 11111111 00000000
+  op = op | DiskID; // 1111 11111111 11111111  
+  op = op << 12; // 1111 11111111 11111111 000000000000 
+  return op;
+}
+
+
 int mdadm_mount(void) {
-  // Complete your code here
+
+  if (mounted_status == 1){
+    return -1;
+  } 
+  
   return 0;
 }
 
@@ -15,7 +41,8 @@ int mdadm_unmount(void) {
 }
 
 int mdadm_read(uint32_t start_addr, uint32_t read_len, uint8_t *read_buf)  {
-  //Complete your code here
+  //Complete your code here, take from the start_addr for read_len and move to read_buf
+ 
   return 0;
 }
 
