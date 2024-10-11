41fd0dfe905f4b079785fbf733fee37c3b2f23d1
Sidhanth Karpe <sjk6612@psu.edu>
2024-10-11 14:41:50 -0400

    edit 11

--- mdadm.c
+++ mdadm.c
@@ -63,15 +63,16 @@ int mdadm_unmount(void) {
 
 int mdadm_read(uint32_t start_addr, uint32_t read_len, uint8_t *read_buf)  {
   //Complete your code here, take from the start_addr for read_len and move to read_buf
-  int bytes_left = read_len;
   uint32_t Disk = start_addr / JBOD_DISK_SIZE; // starting disk
   uint32_t Block = (start_addr % JBOD_DISK_SIZE) / JBOD_BLOCK_SIZE; // starting block
-  uint32_t offset = (start_addr % JBOD_BLOCK_SIZE); // where in the block you start
-  uint32_t *end_addr = (start_addr + read_len);
-  uint32_t *curr_addr = start_addr;
+  uint32_t offset = (start_addr % JBOD_BLOCK_SIZE); // starting byte
+
+  uint32_t bytes_left = read_len;
+  uint32_t bytes_already_read = 0;
+  uint32_t *curr_addr = &start_addr;
+  uint32_t end_addr = start_addr + read_len;
+  uint8_t temp_buffer[JBOD_BLOCK_SIZE];
   
-  // uint32_t *temp_block = NULL;
-  uint8_t temp_buffer[256];
 
   // edge cases: start address is greater than 1mb, end address is start address + read_len 
   if (end_addr > 1048576){ // if address space exceeds 1mb
@@ -90,25 +91,40 @@ int mdadm_read(uint32_t start_addr, uint32_t read_len, uint8_t *read_buf)  {
 
 // 000000000000 11111111 11111111 1111 
   
-  while (bytes_left > 0){
+  while (bytes_left>0){
       
-    uint32_t Curr_Disk = create_op(Disk, 0, JBOD_SEEK_TO_DISK, 0); // starting address
-    uint32_t Curr_Block = create_op(0, Block, JBOD_SEEK_TO_BLOCK, 0); // starting address 
-    jbod_operation(Curr_Disk, NULL);
-    jbod_operation(Curr_Block, NULL);
+    uint32_t Curr_Disk = create_op(Disk, 0, JBOD_SEEK_TO_DISK, 0); // current address
+    uint32_t Curr_Block = create_op(0, Block, JBOD_SEEK_TO_BLOCK, 0); // current address 
+    jbod_operation(Curr_Disk, NULL); 
+    jbod_operation(Curr_Block, NULL); 
 
-    uint32_t read = create_op(Disk, Block, JBOD_READ_BLOCK, 0);
-    uint32_t op = jbod_operation(read, temp_buffer); // copies data and puts it in the 
+    uint32_t read = create_op(Disk, Block, JBOD_READ_BLOCK, 0); // create reading operation
+    uint32_t op = jbod_operation(read, temp_buffer); // reads the entire block and puts it in temp buf, increasing block by 1
     if (op == -1){
       return -1;
     }
-    curr_addr += (JBOD_BLOCK_SIZE - offset);
-    memcpy(read_buf, temp_buffer + offset, JBOD_BLOCK_SIZE - offset);
-    read_buf += (JBOD_BLOCK_SIZE - offset);
 
-    if (curr_addr + offset < end_addr){
-      Disk = 
+    //
+    uint32_t block_bytes = JBOD_BLOCK_SIZE - offset; // bytes within the block 
+    if (bytes_left < block_bytes){ // if end addr is in the same block, 
+      block_bytes = bytes_left;
     }
+
+    curr_addr += (JBOD_BLOCK_SIZE - offset); // move current address to the end of where you just read in the block
+
+
+    if ( offset < end_addr % JBOD_BLOCK_SIZE  && read_len < JBOD_BLOCK_SIZE){ // if you are within a block: start end is less than 
+      memcpy(read_buf, temp_buffer + offset, offset+read_len); // put what you need into the read buf
+      read_buf += (JBOD_BLOCK_SIZE - offset); // add the read block by the size needed
+
+      bytes_left = 0;
+    }
+    // if you are switching blocks 
+
+    if (Block == JBOD_NUM_BLOCKS_PER_DISK){ // if you're at the last block, go to the next disk
+      Disk++;
+    }
+
   }
   return read_len;
 }
